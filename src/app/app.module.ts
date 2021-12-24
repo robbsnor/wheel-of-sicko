a59de9351bb3b0b6b06affa88c361664ba3644c4
robbsnor <robbinschmidt@gmail.com>
2021-12-24 23:30:32 +0100

    component and module folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import { WheelComponent } from './wheel/wheel.component';
-import { DrawerComponent } from './drawer/drawer.component';
-import { IconsModule } from './icons/icons.module';
+import { ListComponent } from './components/list/list.component';
+import { WheelComponent } from './components/wheel/wheel.component';
+import { DrawerComponent } from './components/drawer/drawer.component';
+import { IconsModule } from './modules/icons/icons.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent,
-    WheelComponent,
-    DrawerComponent
-],
-imports: [
-    BrowserModule,
-    IconsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ListComponent,
+        WheelComponent,
+        DrawerComponent
+    ],
+    imports: [
+        BrowserModule,
+        IconsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
