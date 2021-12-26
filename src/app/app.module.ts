7b7bc433f4ab3842bec95952c3a6a27931987cea
robbsnor <robbinschmidt@gmail.com>
2021-12-26 04:32:41 +0100

    redirect to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ const appRoutes: Routes = [
     { path: '', component: IndexComponent },
     { path: 'about', component: AboutComponent },
     { path: 'confetti', component: ConfettiComponent },
-    { path: '**',  component: IndexComponent }
+    { path: '**',  redirectTo: '' }
 ];
 
 @NgModule({
