6b8c17fcea0e0d771ff9d01520a56dc7e9ccf03f
robbsnor <robbinschmidt@gmail.com>
2021-12-26 04:38:32 +0100

    more confetti

--- src/app/pages/confetti/confetti.component.ts
+++ src/app/pages/confetti/confetti.component.ts
@@ -17,29 +17,33 @@ export class ConfettiComponent implements OnInit {
 
     doTheConfetti(): void {
         const colors = ['#ffd900', '#ff002b', '#ffffff'];
+        const duration = 10 * 1000;
+        const end = Date.now() + duration;
 
         const myConfetti = confetti.create(this.ConfettiCanvas, {
             resize: true,
             useWorker: true
         });
 
-        myConfetti({
-            particleCount: 200,
-            angle: 60,
-            spread: 55,
-            origin: {
-                x: 0
-            },
-            colors: colors
-        });
-        myConfetti({
-            particleCount: 200,
-            angle: 120,
-            spread: 55,
-            origin: {
-                x: 1
-            },
-            colors: colors
-        });
+        (function frame() {
+            myConfetti({
+              particleCount: colors.length,
+              angle: 60,
+              spread: 55,
+              origin: { x: 0 },
+              colors: colors
+            });
+            myConfetti({
+              particleCount: colors.length,
+              angle: 120,
+              spread: 55,
+              origin: { x: 1 },
+              colors: colors
+            });
+
+            if (Date.now() < end) {
+              requestAnimationFrame(frame);
+            }
+          }());
     }
 }
